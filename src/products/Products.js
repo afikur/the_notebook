0cc6ba39726e9c1c8cb0ae2a67b88313d882aec0
Afikur Rahman <this.afikur@gmail.com>
2019-07-07 20:28:55 +0600

    add ui dependencies and add products component

---
+++ src/products/Products.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Products extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg-4">Left</div>
+        <div className="col-lg-8">Right</div>
+      </div>
+    );
+  }
+}
+
+export default Products;
+
